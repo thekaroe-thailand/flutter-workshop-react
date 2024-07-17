a6ef4a17163fd61d1c8c80b0f813afcace89255a
thekaroe-thailand <thekaroe@gmail.com>
2024-07-17 22:12:37 +0700

    day23

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <SignIn />,
   },
+  {
+    path: '/home',
+    element: <Home />
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
