d0d400b8e969857e16c4d619599e1d6ac6df3867
thekaroe-thailand <thekaroe@gmail.com>
2024-07-22 22:08:59 +0700

    day 26

--- src/Room.jsx
+++ src/Room.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MyModal from "./components/MyModal";
 import Template from "./Template";
 import Swal from "sweetalert2";
@@ -8,6 +8,7 @@ import config from "./config";
 function Room() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
+  const [rooms, setRooms] = useState([]);
 
   const handleSave = async () => {
     try {
@@ -19,6 +20,72 @@ function Room() {
         config.apiPath + "/api/room/create",
         payload
       );
+
+      if (res.data.message === "success") {
+        Swal.fire({
+          title: "save",
+          text: "save success",
+          icon: "success",
+          timer: 1000,
+        });
+        fetchData();
+        document.getElementById("modalRoom_btnClose").click();
+      }
+    } catch (e) {
+      Swal.fire({
+        title: "error",
+        text: e.message,
+        icon: "error",
+      });
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const res = await axios.get(config.apiPath + "/api/room/list");
+
+      if (res.data.results !== undefined) {
+        setRooms(res.data.results);
+      }
+    } catch (e) {
+      Swal.fire({
+        title: "error",
+        text: e.message,
+        icon: "error",
+      });
+    }
+  };
+
+  // onLoad
+  useEffect(() => {
+    /*
+    ดึงข้อมูลทุกๆ 1000ms = 1 วินาที
+    setTimeout(() => {
+      fetchData();
+    }, 1000);
+    */
+    fetchData();
+  }, []);
+
+  const handleDelete = async (item) => {
+    try {
+      const button = await Swal.fire({
+        title: "Delete Room ?",
+        text: "Are you sure",
+        icon: "question",
+        showCancelButton: true,
+        showConfirmButton: true,
+      });
+
+      if (button.isConfirmed) {
+        const res = await axios.delete(
+          config.apiPath + "/api/room/remove/" + item.id
+        );
+
+        if (res.data.message === "success") {
+          fetchData();
+        }
+      }
     } catch (e) {
       Swal.fire({
         title: "error",
@@ -39,6 +106,41 @@ function Room() {
         <i className="fa fa-plus me-2"></i>
         New Record
       </button>
+
+      <table className="mt-3 table table-bordered">
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>ชื่อห้องพัก</th>
+            <th>ราคาห้อง</th>
+            <th width="110px"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {rooms.length > 0 ? (
+            rooms.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td className="text-center">
+                  <button className="btn btn-primary me-1">
+                    <i className="fa fa-pencil"></i>
+                  </button>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item)}
+                  >
+                    <i className="fa fa-times"></i>
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <></>
+          )}
+        </tbody>
+      </table>
 
       <MyModal id="modalRoom" title="จัดการห้องพัก">
         <div>ชื่อห้องพัก</div>
